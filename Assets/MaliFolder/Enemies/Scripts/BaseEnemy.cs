884bfacb5c233611fec9bf7dde5181e212a90322
MAli-Yildirim <mehmetali98y@gmail.com>
2024-05-12 11:24:27 +0300

    EnemySystemUpdate

--- Assets/MaliFolder/Enemies/Scripts/BaseEnemy.cs
+++ Assets/MaliFolder/Enemies/Scripts/BaseEnemy.cs
@@ -8,14 +8,38 @@ public class BaseEnemy : MonoBehaviour
     public int Hp = 110;
     private int dmg = 210;
 
+    public GameObject PointsObject;
+    
   
 
     public void MoveToPoint(Vector3 CurrentPosition, Vector3 Destination, float speed){
 
-        transform.position = Vector3.Lerp(CurrentPosition,Destination,speed*Time.deltaTime);
+
+        
+
+        transform.position = Vector3.Slerp(CurrentPosition,Destination,speed*Time.deltaTime);
         
 
     }
+
+
+    
+
+    public Vector3[] ConvertPoints(){
+
+        int i = PointsObject.transform.childCount;
+        Vector3[] Points = new Vector3[i];
+        
+
+        for(int j = 0;j!=i;j++){
+            Points[j] = PointsObject.transform.GetChild(j).transform.position;
+        }
+
+        return Points;
+
+    }
+
+
 
 
 
--- Assets/MaliFolder/Enemies/Scripts/LowHpExtradimensionalEnemyBehaviour.cs
+++ Assets/MaliFolder/Enemies/Scripts/LowHpExtradimensionalEnemyBehaviour.cs
@@ -8,13 +8,22 @@ public class LowHpExtradimensionalEnemyBehaviour : BaseEnemy
     private Vector3[] Direction = new [] { new Vector3(0,4,7),new Vector3(0,1,-7),new Vector3(0,6,3),new Vector3(0,-6,6)};
 
     private int i = 0;
+    private Vector3[] Points; 
+    
 
    
     public LowHpExtradimensionalEnemyBehaviour() : base() 
     {}
 
  
-    
+    void Start()
+    {
+
+       Points = ConvertPoints();
+
+
+
+    }
 
 
 
@@ -24,12 +33,18 @@ public class LowHpExtradimensionalEnemyBehaviour : BaseEnemy
             
         
 
-        MoveToPoint(transform.position,Direction[i],1);
+        MoveToPoint(transform.position,Points[i],1);
         
 
-        if((Direction[i] - transform.position).sqrMagnitude < 0.1 )
+        if((Points[i] - transform.position).sqrMagnitude < 0.5 )
         {
             i++;
+
+            if(i>=Points.Length)
+            {
+                i = 0;
+            }
+                        
         }
 
       
